e7524cd677fe1b7ed20937c62421457c1e9e7a65
Mohit Goyal <mohit.work2002@gmail.com>
2023-03-20 14:58:16 +0530

    Our Code

--- client/src/Components/BgVideoSection.js
+++ client/src/Components/BgVideoSection.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import Video from "../Video/bgVideo.mp4"
 
 const BgVideoSection = () => {
   return (
     <>
-        <iframe
+        {/* <iframe
             src={`https://rr1---sn-h557sn6z.googlevideo.com/videoplayback?expire=1679318820&ei=xAoYZJ-9JNvox_AP5fOxyAM&ip=5.254.83.149&id=o-ABE40Dw-jZdGkKLlqjK7lQ23KQsHrVNSaPSGw3LagAgQ&itag=248&source=youtube&requiressl=yes&spc=H3gIhivxWeUkquHT562U9Q9WwsJxov4&vprv=1&svpuc=1&mime=video%2Fwebm&gir=yes&clen=16882109&dur=161.562&lmt=1661884420650476&keepalive=yes&fexp=24007246&c=ANDROID&txp=6319224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgQFoZeI2DBmAL459hyirfyx3uem32oaXMTFm4T5sqQF4CIQCGUKlpk16QW24jY7LSx149UoKbNoZvNRT9MG0w1wMEHQ%3D%3D&title=DJI%20Avata%20Real%20Estate%20Tour%20%7C%20Steady%20Does%20It&redirect_counter=1&rm=sn-4g5ezd7z&req_id=26accdbf32caa3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=kb&mip=117.250.3.86&mm=31&mn=sn-h557sn6z&ms=au&mt=1679295495&mv=u&mvi=1&pl=16&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAOtiUPRO9zX0Nzgp_7-jeHsX2EUq3oI9l2IbqPIvlOhqAiAfJwG39LhVgzNzB-CLv9gnbfFNXHn1qKzyTjr9vWEQLw%3D%3D`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
-        />
+        /> */}
+        {/* <video autoplay muted loop id="myVideo">
+        <source src={Video} type="video/mp4" />
+        Your browser does not support HTML5 video.
+        </video> */}
+        <img src="/img/bgImg.jpg" />
     </>
   )
 }
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -88,74 +88,47 @@ function Home(props) {
             </AppBar>
             <main className='bgVideoDiv'>
                 {/* Hero unit */}
-                <BgVideoSection />
-                <div className={classes.heroUnit}>
-                    <div className={classes.heroContent}>
-                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                            Album layout
+                {/* <BgVideoSection /> */}
+                <div className={`${classes.heroUnit} heroSectionBg`}>
+                    <div className={`${classes.heroContent} ftTxtDiv`}>
+                        <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
+                            BlockEstate
                         </Typography>
                         <Typography variant="h6" align="center" color="textSecondary" paragraph>
-                            Something short and leading about the collection below—its contents, the creator, etc.
-                            Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-                            entirely.
+                        Our Decentralized Land Registry System is a revolutionary platform that allows users to register and list their land in a secure and decentralized manner. With our Dapp, you can enjoy hassle-free land ownership and transfer that is completely transparent, secure, and efficient. We believe that this is the future of land ownership, and we are proud to be at the forefront of this technological revolution.
                         </Typography>
                         <div className={classes.heroButtons}>
                             <Grid container spacing={16} justify="center">
                                 <Grid item>
                                     <Button variant="contained" color="primary">
-                                        Main call to action
+                                        Register To BUY/SELL
                                     </Button>
                                 </Grid>
-                                <Grid item>
+                                {/* <Grid item>
                                     <Button variant="outlined" color="primary">
                                         Secondary action
                                     </Button>
-                                </Grid>
+                                </Grid> */}
                             </Grid>
                         </div>
                     </div>
                 </div>
-                <div className={classNames(classes.layout, classes.cardGrid)}>
-                    {/* End hero unit */}
-                    <Grid container spacing={40}>
-                        {cards.map(card => (
-                            <Grid item key={card} sm={6} md={4} lg={3}>
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                                        title="Image title"
-                                    />
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Heading
-                                        </Typography>
-                                        <Typography>
-                                            This is a media card. You can use this section to describe the content.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="primary">
-                                            View
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Edit
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </div>
+               
             </main>
             {/* Footer */}
             <footer className={classes.footer}>
-                <Typography variant="h6" align="center" gutterBottom>
+                {/* <Typography variant="h6" align="center" gutterBottom>
                     Footer
-                </Typography>
-                <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                    Something here to give the footer a purpose!
-                </Typography>
+                </Typography> */}
+                {/* <small>Made in India with ❤️</small>
+                <Typography  align="center" color="textSecondary">
+                © Copyright 2023 Department of Information Technology & Communication.
+All Rights Reserved
+                </Typography> */}
+                <div className='footerNotes'>
+                    <small>Made in <b>INDIA</b> with ❤️</small>
+                    <small>© Copyright 2023 Department of Information Technology & Communication. All Rights Reserved</small>
+                </div>
             </footer>
             {/* End footer */}
         </React.Fragment>
